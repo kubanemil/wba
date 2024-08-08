626179082c5058c9b791d3dc220bda312951ddd7
kubanemil <emil3toktobekov@gmail.com>
2024-08-08 10:16:23 +0500

    solana-starter

---
+++ solana-starter/ts/cluster1/nft_metadata.ts
@@ -0,0 +1,45 @@
+import wallet from "../wba-wallet.json"
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
+import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
+
+// Create a devnet connection
+const umi = createUmi('https://api.devnet.solana.com');
+
+let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const signer = createSignerFromKeypair(umi, keypair);
+
+umi.use(irysUploader());
+umi.use(signerIdentity(signer));
+
+(async () => {
+    try {
+        // Follow this JSON structure
+        // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
+
+        const image = "https://arweave.net/W3ibdiICmKhtl1E5jO39E5Prnen8qYIHusTzA9lyFnE"
+        const metadata = {
+            name: "EmilRug",
+            symbol: "ERug",
+            description: "MyRug NFT",
+            image: image,
+            attributes: [
+                {trait_type: '?', value: '?'}
+            ],
+            properties: {
+                files: [
+                    {
+                        type: "image/png",
+                        uri: image
+                    },
+                ]
+            },
+            creators: []
+        };
+        const myJsonMetadata = await umi.uploader.uploadJson(metadata);
+        console.log("Your metadata URI: ", myJsonMetadata); // https://arweave.net/peuhn6bKIfWiE_fb0BcIjp7m41cgDAsFcAcBmtdlBJU
+    }
+    catch(error) {
+        console.log("Oops.. Something went wrong", error);
+    }
+})();
