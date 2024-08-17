2bd519709aee359163937995eab0146565d0ffa9
kubanemil <emil3toktobekov@gmail.com>
2024-08-17 17:43:49 +0500

    solana-starter vault deposit/withdraw

--- solana-starter/ts/cluster1/vault_deposit.ts
+++ solana-starter/ts/cluster1/vault_deposit.ts
@@ -30,28 +30,26 @@ const provider = new AnchorProvider(connection, new Wallet(keypair), {
 });
 
 // Create our program
-const program = new Program<WbaVault>(IDL, "<address>" as Address, provider);
-
-// Create a random keypair
-const vaultState = new PublicKey("<address>");
-// Create the PDA for our enrollment account
-// const vaultAuth = ???
-
-// Create the vault key
-// const vault = ???
+const program = new Program<WbaVault>(IDL, "D51uEDHLbWAxNfodfQDv7qkp8WZtxrhi3uganGbNos7o" as Address, provider);
+const vaultState = new PublicKey("A7GTzaUzcUMEPnUK4ptM97MnDJEM7N4dNVC2NkNKPWsd");
+const vaultAuth = PublicKey.findProgramAddressSync([Buffer.from("auth"), vaultState.toBuffer()], program.programId)[0];
+const vault = PublicKey.findProgramAddressSync([Buffer.from("vault"), vaultAuth.toBuffer()], program.programId)[0];
 
 // Execute our enrollment transaction
 (async () => {
   try {
-    // const signature = await program.methods
-    // .deposit(new BN(<number>)    )
-    // .accounts({
-    //    ???
-    // })
-    // .signers([
-    //     keypair
-    // ]).rpc();
-    // console.log(`Deposit success! Check out your TX here:\n\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`);
+    const signature = await program.methods
+      .deposit(new BN(1000_000_000)) // 1 SOL
+      .accounts({
+        owner: keypair.publicKey,
+        vaultState: vaultState,
+        vaultAuth: vaultAuth,
+        vault: vault,
+      })
+      .signers([
+        keypair
+      ]).rpc(); // 4UGYPvjKcEQahktxea9qNoc3c82cFAeU2BgDmoAUoaUGuWzgrpxRKGAoY2cpXbBxuPHQRtNBB8oZLLUMe9tcUVwL
+    console.log(`Deposit success! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`);
   } catch (e) {
     console.error(`Oops, something went wrong: ${e}`);
   }
--- solana-starter/ts/cluster1/vault_init.ts
+++ solana-starter/ts/cluster1/vault_init.ts
@@ -8,7 +8,6 @@ import {
 import { Program, Wallet, AnchorProvider, Address } from "@coral-xyz/anchor";
 import { WbaVault, IDL } from "./programs/wba_vault";
 import wallet from "./wallet/wba-wallet.json";
-/// J8qKEmQpadFeBuXAVseH8GNrvsyBhMT8MHSVD3enRgJz
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -33,24 +32,29 @@ const program = new Program<WbaVault>(
 
 // Create a random keypair
 const vaultState = Keypair.generate();
-console.log(`Vault public key: ${vaultState.publicKey.toBase58()}`);
+console.log(`Vault public key: ${vaultState.publicKey.toBase58()}`); // A7GTzaUzcUMEPnUK4ptM97MnDJEM7N4dNVC2NkNKPWsd
 
 // Create the PDA for our enrollment account
 // Seeds are "auth", vaultState
-// const vaultAuth = ???
+const vaultAuth = PublicKey.findProgramAddressSync([Buffer.from("auth"), vaultState.publicKey.toBuffer()], program.programId)[0];
 
 // Create the vault key
 // Seeds are "vault", vaultAuth
-// const vault = ???
+const vault = PublicKey.findProgramAddressSync([Buffer.from("vault"), vaultAuth.toBuffer()], program.programId)[0];
 
 // Execute our enrollment transaction
 (async () => {
   try {
-    // const signature = await program.methods.initialize()
-    // .accounts({
-    //     ???
-    // }).signers([keypair, vaultState]).rpc();
-    // console.log(`Init success! Check out your TX here:\n\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`);
+    const signature = await program.methods.initialize()
+    .accounts({
+        owner: keypair.publicKey,
+        vaultState: vaultState.publicKey,
+        vaultAuth: vaultAuth,
+        vault: vault,
+        // systemProgram: SystemProgram.programId,
+        
+    }).signers([keypair, vaultState]).rpc(); // 4L1rKEjTAJckv7PWp6p1MewoB5JGVgvwmyFWJe5ud5mzvkWwYCpW1Av4kM7azZMZeKyhrz7EhgtJLV9Ch6q1sbd6
+    console.log(`Init success! Check out your TX here: \nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`);
   } catch (e) {
     console.error(`Oops, something went wrong: ${e}`);
   }
